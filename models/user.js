9ac29aada686e3c25c29428ed1f980a5f02cc701
leonaysmael16 <ysmealleona@gmail.com>
2021-12-12 00:08:24 -0800

    added user.js

--- models/user.js
+++ models/user.js
@@ -8,4 +8,37 @@ class User extends Model {
     }
 }
 
-User.init
+User.init(
+    {
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    }
+},
+{
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    }
+},
+{
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: { isEmail: true },
+        unique: true,
+    }
+},
+{
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: { len: [5] },
+    },
+},
+{
+    
+}
+)
